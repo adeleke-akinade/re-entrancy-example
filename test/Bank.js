dbc1d28083e2b284b2c95abb3244e48a52611d73
Adeleke Akinade <adeleke.akinade@aat.org.uk>
2023-09-02 20:06:38 +0100

    Improves the unit tests.

--- test/Bank.js
+++ test/Bank.js
@@ -4,10 +4,10 @@ const { expect } = require("chai");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 async function main() {
-    async function deployVulnerableContractFixture() {
+    async function deployContracts(bank) {
         const accounts = await ethers.getSigners();
         const signer = accounts[0];
-        const bankFactory = await ethers.getContractFactory("VulnerableBank", signer);
+        const bankFactory = await ethers.getContractFactory(bank, signer);
         const bankContract = await bankFactory.deploy();
         await bankContract.waitForDeployment();
         const exploiterFactory = await ethers.getContractFactory("Exploiter", signer);
@@ -17,8 +17,20 @@ async function main() {
         return { bankContract, exploiterContract, accounts };
     }
 
+    async function deployVulnerableContractFixture() {
+        return await deployContracts("VulnerableBank");
+    }
+
+    async function deploySecureContractFixture() {
+        return await deployContracts("SecureBank");
+    }
+
+    async function deployBetterSecureContractFixture() {
+        return await deployContracts("BetterSecureBank");
+    }
+
     describe("VulnerableBank", async function () {
-        it("Should withdraw all the funds to the exploiter contract", async function () {
+        it("Should drain all the ether from the Bank contract", async function () {
             const { bankContract, exploiterContract, accounts } = await loadFixture(deployVulnerableContractFixture);
 
             await (async function () {
@@ -33,6 +45,32 @@ async function main() {
 
             expect(await bankContract.getContractBalance()).to.equal(0);
             expect(await exploiterContract.getContractBalance()).to.equal(ethers.parseEther("6"));
+        });
+    });
+
+    describe("SecureBank", async function () {
+        it("Should revert with correct message when the Exploiter contract attempts to drain the ether from the Bank contract", async function () {
+            const { bankContract, exploiterContract, accounts } = await loadFixture(deploySecureContractFixture);
+
+            await (async function () {
+                const tx = await bankContract.deposit({ value: ethers.parseEther("5") });
+                await tx.wait();
+            })();
+
+            await expect(exploiterContract.launchExploit({ value: ethers.parseEther("1") })).to.be.revertedWith("Bank: failed to send funds");
+        });
+    });
+
+    describe("BetterSecureBank", async function () {
+        it("Should revert with correct message when the Exploiter contract attempts to drain the ether from the Bank contract", async function () {
+            const { bankContract, exploiterContract, accounts } = await loadFixture(deployBetterSecureContractFixture);
+
+            await (async function () {
+                const tx = await bankContract.deposit({ value: ethers.parseEther("5") });
+                await tx.wait();
+            })();
+
+            await expect(exploiterContract.launchExploit({ value: ethers.parseEther("1") })).to.be.revertedWith("Bank: failed to send funds");
         });
     });
 }
